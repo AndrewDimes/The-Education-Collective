93cabc854181f7633d0091edc379471de1528da8
Andrew Dimes <andrewdimes@gmail.com>
2022-06-26 12:06:08 -0700

    First attempt to add Thiens changes. Currently not working

---
+++ insert.js
@@ -0,0 +1,10 @@
+const insertPost = require('./models/postModel')
+
+ const insertPost2 = new insertPost({
+          name: 'Thien',
+          title: 'test',
+          description: 'test description',
+          author: 'author3'
+        })
+
+  insertPost2.save();
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,27 @@
 const router = require('express').Router();
 const usersCtrl = require('../controllers/users');
-const user = require('../models/user');
+//const addPostCtrlOne = require('../controllers/addPostController');
+//const addPostTestCtrl = require('../controllers/addPostTestController');
+const addCreateDataCtrl = require('../controllers/createController');
+const addSuccessPostCtrl = require('../controllers/successController');
+const fetchController= require('../controllers/fetch-controller');
+
+
 
 router.get('/users', usersCtrl.index);
+//router.get('/addPosts', addPostCtrlOne.addPost);
+//router.get('/addPostsTestView', addPostTestCtrl.addPostTestController);
+router.post('/create', addCreateDataCtrl.createData);
+router.get('/userForm', addCreateDataCtrl.userForm);
+router.get('/SuccessPost3', addSuccessPostCtrl.successController);
+router.get('/fetch-data',fetchController.fetchData);
 
 
-function isLoggedIn(req, res, next){
+function isLoggedIn(req, res, next)
+{
     if(req.isAuthenticated()) return next()
     res.redirect('/auth/google')
 }
+
 
 module.exports = router;
